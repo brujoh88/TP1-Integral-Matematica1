09cf06ffd8dc40dbcc8d1074c1a1dae5819c9e8a
SantiZ2610 <santiagoemanuel_2000@live.com.ar>
2025-04-23 12:08:30 -0300

    Se agrego sistema decimal a octal

--- santi.py
+++ santi.py
@@ -1,11 +1,27 @@
 import recursos
-
+import david  # Asegúrate que david.py tiene la función convertir_decimal_a_eleccion
 
 def decimal_a_octal():
-    """Convierte un número decimal a octal"""
     recursos.limpiar_pantalla()
-    print("==== DECIMAL A OCTAL ====")  
-    
-    
+    print("""==== SISTEMA DECIMAL A OCTAL ====
+\nIngrese el número entero que desea convertir a octal:""")  
+
+    try: 
+        numero = int(input())
+
+        if numero < 0:
+            print("\nError. Por favor, ingrese un número entero positivo.")
+            recursos.pausar()
+            return
+
+        if numero == 0:
+            octal = "0"
+        else:
+            octal = david.convertir_decimal_a_eleccion(8, numero)
+
+        print(f"\nEl número ingresado es {octal} en octal.")
+    except ValueError:
+        print("\nError. Valor incorrecto.")   
+
     recursos.pausar()
 
