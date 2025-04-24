90d474b085db4c817c8a79f7044b92018a6c7c17
FlorenciaVivas <florxviv@gmail.com>
2025-04-23 21:49:19 -0300

    Funcion suma completada

--- flor.py
+++ flor.py
@@ -1,9 +1,27 @@
 import recursos
+import david
 
-def binario_a_complemento_dos():
-    """Convierte un número binario a su complemento a dos"""
+def suma_binarios():
+    """Suma de numeros binarios"""
     recursos.limpiar_pantalla()
-    print("==== BINARIO A COMPLEMENTO A DOS ====")
+    print("==== SUMA DE NUMEROS BINARIOS ====")
+    bin1 = input("Ingrese el primer número binario: ")
+    bin2 = input("Ingrese el segundo número binario: ")
 
-    
+    try:
+        # Convertimos los binarios a enteros
+        num1 = int(bin1, 2)
+        num2 = int(bin2, 2)
+
+#Sumamos los enteros
+        suma = num1 + num2
+
+#Convertimos el resultado a binario
+        resultado_binario = david.convertir_decimal_a_eleccion(2, suma)
+
+        print(f"La suma de {bin1} + {bin2} es: {resultado_binario}")
+    except ValueError:
+        print("Error: uno o ambos valores no son números binarios válidos.")
+
+
     recursos.pausar()
